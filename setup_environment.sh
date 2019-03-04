4deaf275ea77996f5f04beacc5c1a59a9be4f7c1
eruditelite <eruditelite@gmail.com>
2019-03-04 10:27:10 -0600

    Commands are Now in README.md

--- setup_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use 'environment'
-rm $HOME/.profile
-ln -s $HOME/environment/login $HOME/.profile
-rm $HOME/.bashrc
-ln -s $HOME/environment/environment $HOME/.bashrc
-rm $HOME/.bash_logout
-ln -s $HOME/environment/logout $HOME/.bash_logout
